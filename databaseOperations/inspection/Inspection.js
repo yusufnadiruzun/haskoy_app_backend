cff440b1d85e9a22a559970a91e7e596d1903791
Yusuf Nadir Uzun <yusuf.nadir94@gmail.com>
2024-08-26 12:10:31 +0300

    update code added to  addInspecton

--- databaseOperations/inspection/Inspection.js
+++ databaseOperations/inspection/Inspection.js
@@ -19,7 +19,54 @@ const createInspectionDb = (inspection_name) => {
     });
   });
 };
+const addInspectionDb = (inspection_name, student_phone, status) => {
+  let student_id = "";
+  let inspection_type_id = "";
 
+  return new Promise((resolve, reject) => {
+    let query = `select * from inspection_type where inspection_name = ('${inspection_name}');`;
+    connection.query(query, function (err, result) {
+      if (err) throw err;
+      if (result.length == 0) {
+        reject("inspection_name does not exist");
+      } else {
+        inspection_type_id = result[0].inspection_type_id;
+        query = `select * from student where phone = ('${student_phone}');`;
+        connection.query(query, function (err, result) {
+          if (err) throw err;
+          if (result.length == 0) {
+            reject("phone does not exist");
+          } else {
+            student_id = result[0].student_id;
+
+            query = `select * from inspection where inspection_type_id = '${inspection_type_id}' and student_id = '${student_id}' and date = '${TodayDate()}';`;
+            connection.query(query, function (err, result) {
+              if (err) throw err;
+
+              if (result.length > 0) {
+                // Mevcut ise status güncelleme işlemi
+                query = `UPDATE inspection SET status = '${status}' WHERE inspection_type_id = '${inspection_type_id}' AND student_id = '${student_id}' AND date = '${TodayDate()}';`;
+                connection.query(query, function (err, result) {
+                  if (err) throw err;
+                  return resolve("Yoklama güncellendi");
+                });
+              } else {
+                // Yoklama mevcut değilse yeni bir kayıt ekleme işlemi
+                query = `INSERT INTO inspection(inspection_type_id, student_id, date, status) VALUES ('${inspection_type_id}', '${student_id}', '${TodayDate()}', '${status}');`;
+                connection.query(query, function (err, result) {
+                  if (err) throw err;
+                  return resolve("Yoklama eklendi");
+                });
+              }
+            });
+          }
+        });
+      }
+    });
+  });
+};
+
+/*
 const addInspectionDb = (inspection_name, student_phone, status) => {
   let student_id = "";
   let inspection_type_id = "";
@@ -60,6 +107,7 @@ const addInspectionDb = (inspection_name, student_phone, status) => {
     });
   });
 };
+*/
 
 const deleteInspectionDb = (inspection_name, student_phone, date) => {
   let student_id = "";
